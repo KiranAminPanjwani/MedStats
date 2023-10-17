58f57cb5e57a80d82a3c4513e8baa0423aefe23a
thesunny-dev <59652037+thesunny-dev@users.noreply.github.com>
2023-10-17 17:50:15 +0900

    temp

---
+++ js/saveRecord.js
@@ -0,0 +1,18 @@
+/* Selectors */
+const medicalForm = document.getElementById("MedicalForm");
+const inputName = document.querySelector("#PatientNameInput input");
+const inputSymtom = document.querySelector("#SymptomsInput input");
+const inputMed = document.querySelector("#MedicationsInput input");
+
+const inputBtn = document.getElementById("add-record-btn");
+
+
+const onSave=(e)=>{
+    e.preventDefault();
+    console.log(e.value);
+}
+
+
+
+/* Event Listeners */
+inputBtn.addEventListener("click",(e)=>{onSave(e)});
