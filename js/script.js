15a0e07debaa5724b297261bcd9e2ced972de6bf
thesunny-dev <59652037+thesunny-dev@users.noreply.github.com>
2023-10-19 14:54:45 +0900

    deleted static records

--- js/script.js
+++ js/script.js
@@ -3,40 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputBtn = document.getElementById("MedicalForm");
 
   /** ---- Variables ---- */
-  var staticPatientName = "Patient Name";
-  var staticSymptoms = "Static Symptoms";
-  var staticMedications = "Static Medications";
-  var staticStatus = "Static Status: Excellent"; // You can customize the status as needed
-
-  // Create a record element for the static data
-  var staticRecordElement = document.createElement("div");
-  staticRecordElement.className = "record";
-  staticRecordElement.innerHTML = `
-    <div class="record-header">
-        <h5 class="record-date">[Static Date]</h5>
-        <div class="icons">
-        <button id="edit">✏️</button>
-        <button id="close"><img src="./trash-alt-icon-462x512-xs5e5fm6.png" alt="dustbin"></button>
-      </div>
-    </div>
-    <div class="record-body">
-        <h4 class="record-section-title">Patient: ${staticPatientName}</h4>
-        <h4 class="record-section-title">Symptoms</h4>
-        <ul class="record-list">
-            <li>${staticSymptoms}</li>
-        </ul>
-        <h4 class="record-section-title">Medications</h4>
-        <ul class="record-list">
-            <li>${staticMedications}</li>
-        </ul>
-    </div>
-    <div class="record-footer">
-        <h4 class="record-status">${staticStatus}</h4>
-    </div>
-`;
-  // Append the static record to the record container
-  document.querySelector(".record-container").appendChild(staticRecordElement);
-
   let recordObj = [];
   const RECORDOBJ_KEY = "MyRecords";
   /** ---- FUNCTIONS ---- */
